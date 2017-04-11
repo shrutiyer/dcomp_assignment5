6ba17ed1ab9f9a84d34a624e7e2da6502de8753e
David Abrahams <davidabrahams@sbcglobal.net>
2017-04-10 21:41:03 -0400

    finishing reducer

--- src/Reducer.java
+++ src/Reducer.java
@@ -51,6 +51,7 @@ public class Reducer extends UnicastRemoteObject implements iReducer{
     @Override
     public void receiveValues(int value) throws RemoteException {
         // called by the mapper task, receives a word count.
+        System.out.println("Reducer with key " + key + "received value " + value);
         wordCount = wordCount + value;
     }
 
